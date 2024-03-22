6ab5177c3f76ec25beb553431787635b98fc9b4f
Gustavo Barbosa Sanchez <11802440@usp.br>
2024-03-22 17:17:05 -0300

    Correct name header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -108,8 +108,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">The Prism</span>
+            <span className="text-xs">AI | Art | T-shirts</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
