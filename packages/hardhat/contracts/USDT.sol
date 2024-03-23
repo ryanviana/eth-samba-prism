8631c94ccf6847f70bbbc8a26e808ae34643651c
pjvperes <pcamposperes2@gmail.com>
2024-03-23 01:01:07 -0300

    USDT to test

---
+++ packages/hardhat/contracts/USDT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDT is ERC20 {
+    constructor() ERC20("USDT", "USDT") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function decimals() public pure override returns(uint8) {
+        return 6;
+    }
+}
