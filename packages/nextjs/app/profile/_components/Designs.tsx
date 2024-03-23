914ffb58969c97a2de0bf40ee380d9c033052fd8
Gustavo Barbosa Sanchez <11802440@usp.br>
2024-03-23 12:36:25 -0300

    Removed doubled CSS

--- packages/nextjs/app/profile/_components/Designs.tsx
+++ packages/nextjs/app/profile/_components/Designs.tsx
@@ -26,7 +26,7 @@ const Designs = () => {
     <div className="flex flex-col flex-1 items-center">
       <div className="flex flex-col items-start sm:w-[70%] w-full sm:px-0 px-4 py-10">
         <h1 className="text-3xl sm:text-4xl text-gradient pb-4">My Designs (NFTs)</h1>
-        <div className="justify-between items-center flex flex-grow flex-col p-8 gap-4 bg-base-100 rounded-3xl shadow-md shadow-secondary border border-base-300 flex flex-col p-5 w-full">
+        <div className="justify-between items-center flex flex-grow flex-col p-8 gap-4 bg-base-100 rounded-3xl shadow-md shadow-secondary border border-base-300 w-full">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-center items-start">
             {designs.map(design => (
               <NFTCard
